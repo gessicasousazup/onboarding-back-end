cabea4066866f6eeba1b39de0314622548b65fac
Gessicasouza <gessica.sousa@zup.com.br>
2019-11-25 19:08:52 -0300

    primeiro commit

---
+++ src/main/java/br/com/zup/onboarding/OnboardingBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingBackEndApplication.class, args);
+	}
+
+}
