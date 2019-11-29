4f07b93fe984ee24c379b4447de23dda7e2cbe75
Eduardo Pereira Ribeiro <epribeiro@localhost.localdomain>
2019-11-29 08:42:13 -0300

    Adição dos métodos POST e DeleteById na classe Step

--- src/main/java/br/com/zup/onboarding/api/StepController.java
+++ src/main/java/br/com/zup/onboarding/api/StepController.java
@@ -1,6 +1,8 @@
 package br.com.zup.onboarding.api;
 
+import br.com.zup.onboarding.api.Append.Question.QuestionCreate;
 import br.com.zup.onboarding.api.Append.Question.QuestionRepresentation;
+import br.com.zup.onboarding.api.Append.Step.StepCreate;
 import br.com.zup.onboarding.api.Append.Step.StepRepresentation;
 import br.com.zup.onboarding.models.Question;
 import br.com.zup.onboarding.models.Step;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -36,6 +39,29 @@ public class StepController {
             return ResponseEntity.ok(representation);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<StepRepresentation> saveStep(@Valid @RequestBody StepCreate step) {
+        try {
+            Step newStep = stepService.createStep(step);
+            StepRepresentation representation = new StepRepresentation();
+            representation.setId(newStep.getId());
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(representation);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteStep(@PathVariable long id) {
+        try {
+            stepService.deleteStep(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 }
